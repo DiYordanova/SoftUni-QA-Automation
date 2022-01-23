d45f0fd6c3a3f3ca229be2b68ddf94ccaa084b6f
Dilyana <d-yordanova@mail.bg>
2022-01-23 16:17:17 +0200

    Added solution.

---
+++ ApiGitHub-WebServices-And-Integration/API-Tests-GitHub/CommentResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace API_Tests_GitHub
+{
+    class CommentResponse
+    {
+        public long Id { get; set; }
+
+        public string Body { get; set; }
+    }
+}
---
+++ ApiGitHub-WebServices-And-Integration/API-Tests-GitHub/GitHubIssuesTests.cs
@@ -0,0 +1,136 @@
+using NUnit.Framework;
+using RestSharp;
+using RestSharp.Authenticators;
+using RestSharp.Serialization.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace API_Tests_GitHub
+{
+    public class Tests
+    {
+        const string GitHubApiUserName = "DiYordanova";
+        const string GitHubApiPassword = "GitHub_Password";
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test_GitHubAPI_GetIssuesByRepo()
+        {
+            var client = new RestClient("https://api.github.com/repos/testnakov/test-nakov-repo/issues");
+            client.Timeout = 3000;
+            var request = new RestRequest(Method.GET);
+            
+            var response = client.Execute(request);
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+
+            Assert.IsTrue(response.ContentType.StartsWith("application/json"));
+
+            var issues = new JsonDeserializer().Deserialize<List<IssueResponse>>(response);
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test_GitHubAPI_CreateNewIssue()
+        {
+            var client = new RestClient("https://api.github.com/repos/testnakov/test-nakov-repo/issues");
+            client.Timeout = 3000;
+            var request = new RestRequest(Method.POST);
+            client.Authenticator = new HttpBasicAuthenticator(GitHubApiUserName, GitHubApiPassword);
+            request.AddHeader("Content-type", "application/json");
+            request.AddJsonBody(new
+            {
+                title = "some title",
+                body = "some body",
+                label = new string[] { "bug", "importance:high", "type:UI" }
+            }
+            );
+            var response = client.Execute(request);
+
+            Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
+
+            Assert.IsTrue(response.ContentType.StartsWith("application/json"));
+
+            var issues = new JsonDeserializer().Deserialize<IssueResponse>(response);
+
+            Assert.IsTrue(issues.id > 0);
+            Assert.IsTrue(issues.number > 0);
+            Assert.IsTrue(!String.IsNullOrEmpty(issues.title));
+            Assert.IsTrue(!String.IsNullOrEmpty(issues.body));           
+        }
+
+        [Test]
+        public void Test_GitHubAPI_CreateNewIssue_Unauthorized()
+        {
+            var client = new RestClient("https://api.github.com/repos/testnakov/test-nakov-repo/issues");
+            client.Timeout = 3000;
+            var request = new RestRequest(Method.POST);
+            //client.Authenticator = new HttpBasicAuthenticator(GitHubApiUserName, GitHubApiPassword);
+            request.AddHeader("Content-type", "application/json");
+            request.AddJsonBody(new
+            {
+                title = "some title",
+                body = "some body",
+                label = new string[] { "bug", "importance:high", "type:UI" }
+            }
+            );
+            var response = client.Execute(request);
+
+            Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Test]
+        public void Test_GitHubAPI_CreateNewIssue_MissingTitle()
+        {
+            var client = new RestClient("https://api.github.com/repos/testnakov/test-nakov-repo/issues");
+            client.Timeout = 3000;
+            var request = new RestRequest(Method.POST);
+            client.Authenticator = new HttpBasicAuthenticator(GitHubApiUserName, GitHubApiPassword);
+            request.AddHeader("Content-type", "application/json");
+            request.AddJsonBody(new
+            {               
+                body = "some body",
+                label = new string[] { "bug", "importance:high", "type:UI" }
+            }
+            );
+            var response = client.Execute(request);
+
+            Assert.AreEqual(HttpStatusCode.UnprocessableEntity, response.StatusCode);
+        }
+
+        [Test]
+        public void Test_GitHubAPI_DeleateComment()
+        {
+            //Firts create issue comment
+            var client = new RestClient("https://api.github.com/repos/testnakov/test-nakov-repo/issues/3035/comments");
+            client.Timeout = 3000;
+            var request = new RestRequest(Method.POST);
+            client.Authenticator = new HttpBasicAuthenticator(GitHubApiUserName, GitHubApiPassword);
+            request.AddHeader("Content-type", "application/json");
+            request.AddJsonBody(new
+            {                
+                body = "One more comment...."              
+            }
+            );
+            var response = client.Execute(request);
+
+            Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
+
+            var newComment = new JsonDeserializer().Deserialize<CommentResponse>(response);
+
+            //Second delete issue comment
+            var clientDelete = new RestClient("https://api.github.com/repos/testnakov/test-nakov-repo/issues/comments/" + newComment.Id);
+            clientDelete.Timeout = 3000;
+            var requestDelete = new RestRequest(Method.DELETE);
+            clientDelete.Authenticator = new HttpBasicAuthenticator(GitHubApiUserName, GitHubApiPassword);
+            var responseDelete = clientDelete.Execute(requestDelete);
+
+            Assert.AreEqual(HttpStatusCode.NoContent, responseDelete.StatusCode);
+        }
+    }
+}

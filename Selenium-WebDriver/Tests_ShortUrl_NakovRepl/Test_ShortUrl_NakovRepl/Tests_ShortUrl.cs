8e1a5f7f2758ddaa85471bc9ae01f854fe90e4d8
Dilyana Yordanova <d-yordanova@mail.bg>
2022-05-19 01:33:49 +0300

    Added test.

--- Selenium-WebDriver/Tests_ShortUrl_NakovRepl/Test_ShortUrl_NakovRepl/Tests_ShortUrl.cs
+++ Selenium-WebDriver/Tests_ShortUrl_NakovRepl/Test_ShortUrl_NakovRepl/Tests_ShortUrl.cs
@@ -10,14 +10,49 @@ namespace Test_ShortUrl_NakovRepl
     {
         ChromeDriver driver;
 
-        [SetUp]
+        [OneTimeSetUp]
         public void Setup()
         {
             driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
         }
 
         [Test]
-        public void Test_AddUrl()
+        public void Verify_HomePageTitleAndHeader()
+        {
+            driver.Url = "https://shorturl.nakov.repl.co";
+            var urlShortener = driver.FindElement(By.XPath("//h1[contains(.,'URL Shortener')]")).Text;
+
+            Assert.AreEqual("URL Shortener", driver.Title);
+            Assert.AreEqual("URL Shortener", urlShortener);            
+        }
+
+
+        [Test]
+        public void Verify_ShortUrlsTitleAndHeader()
+        {
+            driver.Url = "https://shorturl.nakov.repl.co/urls";
+            var shortUrls = driver.FindElement(By.XPath("//h1[contains(.,'Short URLs')]")).Text;
+
+            Assert.AreEqual("Short URLs", driver.Title);
+            Assert.AreEqual("Short URLs", shortUrls);
+        }
+
+        [Test]
+        public void Verify_ShortUrlsTable()
+        {
+            driver.Url = "https://shorturl.nakov.repl.co/urls";          
+            var pageTitle = driver.FindElement(By.CssSelector("main > h1")).Text;
+            var nakovComField = driver.FindElement(By.XPath("//a[@href='https://nakov.com']")).Text;
+            var shorturlFiels = driver.FindElement(By.XPath("//a[@href='http://shorturl.nakov.repl.co/go/nak']")).Text;
+
+            Assert.AreEqual("Short URLs", pageTitle);
+            Assert.AreEqual("https://nakov.com", nakovComField);
+            Assert.AreEqual("http://shorturl.nakov.repl.co/go/nak", shorturlFiels);
+        }
+
+        [Test]
+        public void AddUrl_ValidData()
         {
             //Add new url
             driver.Url = "https://shorturl.nakov.repl.co/add-url";
@@ -28,7 +63,7 @@ namespace Test_ShortUrl_NakovRepl
             var code = "code" + DateTime.Now.Ticks;
             textBoxCode.Clear();
             textBoxCode.SendKeys(code);
-            var submitButton = driver.FindElement(By.XPath("//form/button[@type='submit']"));
+            var submitButton = driver.FindElement(By.XPath("//button[@type='submit'][contains(.,'Create')]"));
             submitButton.Click();
 
 
@@ -45,7 +80,50 @@ namespace Test_ShortUrl_NakovRepl
             Assert.AreEqual("http://shorturl.nakov.repl.co/go/" + code, secondCellText);
         }
 
-        [TearDown]
+        [Test]
+        public void AddUrl_InvalidData()
+        {            
+            driver.Url = "https://shorturl.nakov.repl.co/add-url";
+            var textBoxUrl = driver.FindElement(By.Id("url"));
+            var newUrl = "99999";
+            textBoxUrl.SendKeys(newUrl);
+            var textBoxCode = driver.FindElement(By.CssSelector("#code"));
+            var code = "code" + DateTime.Now.Ticks;
+            textBoxCode.Clear();
+            textBoxCode.SendKeys(code);
+            var submitButton = driver.FindElement(By.XPath("//button[@type='submit'][contains(.,'Create')]"));
+            submitButton.Click();
+            var errorMassage = driver.FindElement(By.CssSelector("body > div"));
+            Assert.AreEqual("Invalid URL!", errorMassage.Text);            
+        }
+
+        [Test]
+        public void AddUrl_EmptyData()
+        {
+            driver.Url = "https://shorturl.nakov.repl.co/add-url";           
+            var submitButton = driver.FindElement(By.XPath("//button[@type='submit'][contains(.,'Create')]"));
+            submitButton.Click();
+            var errorMassage = driver.FindElement(By.CssSelector("body > div"));
+            Assert.AreEqual("URL cannot be empty!", errorMassage.Text);
+        }
+
+        [Test]
+        public void Chech_VisitorCounterWorkCorrectly()
+        {
+            driver.Url = "https://shorturl.nakov.repl.co/urls";
+            var visitorFieldBeforeClickText = driver.FindElement(By.CssSelector("body td:nth-child(4)")).Text;
+            var visitorFieldBeforeClickNumber = int.Parse(visitorFieldBeforeClickText);
+            var shortUrlField = driver.FindElement(By.CssSelector("body td:nth-child(2) > a"));
+            shortUrlField.Click();
+            driver.SwitchTo().Window(driver.WindowHandles[1]);
+            driver.SwitchTo().Window(driver.WindowHandles[0]);
+            var visitorFieldAfterClickText = driver.FindElement(By.CssSelector("body td:nth-child(4)")).Text;
+            var visitorFieldAfterClickNumber = int.Parse(visitorFieldAfterClickText);
+            var visitorresult = visitorFieldAfterClickNumber + 1;
+            Assert.AreEqual(visitorFieldBeforeClickNumber, visitorFieldAfterClickNumber);
+        }
+
+        [OneTimeTearDown]
         public void ShutDown()
         {
             driver.Quit();

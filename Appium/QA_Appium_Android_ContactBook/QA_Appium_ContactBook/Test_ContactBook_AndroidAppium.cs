109176db717446e9028b061d7fae6c0fb8ac25a5
Dilyana Yordanova <d-yordanova@mail.bg>
2022-05-29 00:11:33 +0300

    Added tests

--- Appium/QA_Appium_Android_ContactBook/QA_Appium_ContactBook/Test_ContactBook_AndroidAppium.cs
+++ Appium/QA_Appium_Android_ContactBook/QA_Appium_ContactBook/Test_ContactBook_AndroidAppium.cs
@@ -3,39 +3,46 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.Android;
 using OpenQA.Selenium.Appium.Enums;
+using OpenQA.Selenium.Support.UI;
 using System;
+using System.Threading;
 
 namespace QA_Appium_ContactBook
 {
     public class Test_ContactBook_AndroidAppium
     {
         private AndroidDriver<AndroidElement> driver;
+        private AppiumOptions options;
+        private const string AppiumURI = "http://127.0.0.1:4723/wd/hub";
+        private const string ApiServiceUrl = "https://contactbook.nakov.repl.co/api";
+        private const string appPath = @"C:\Users\Dyliana\Desktop\SoftUni-QA-Automation\Appium\QA_Appium_Android_ContactBook\contactbook-androidclient.apk";
+        private WebDriverWait wait;
 
         [SetUp]
         public void Setup()
         {
-            var options = new AppiumOptions();
-            options.AddAdditionalCapability(MobileCapabilityType.App, @"C:\Users\35989\source\repos\QA_Appium\QA_Appium_ContactBook\contactbook-androidclient.apk");
+            options = new AppiumOptions();
+            options.AddAdditionalCapability(MobileCapabilityType.App, appPath);
             options.AddAdditionalCapability(MobileCapabilityType.PlatformName, "Android");
-            driver = new AndroidDriver<AndroidElement>(new Uri("http://[::1]:4723/wd/hub"), options);
-
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
+            driver = new AndroidDriver<AndroidElement>(new Uri(AppiumURI), options);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));            
         }
 
         [Test]
-        public void Test_ContactsBookSearch()
+        public void Search_ValidData_SingleResult()
         {            
             var contactBookUrl = driver.FindElement(By.Id(
                 "contactbook.androidclient:id/editTextApiUrl"));
             contactBookUrl.Clear();
-            contactBookUrl.SendKeys("https://contactbook.nakov.repl.co/api");
+            contactBookUrl.SendKeys(ApiServiceUrl);
 
             var buttonConnect = driver.FindElement(By.Id(
                 "contactbook.androidclient:id/buttonConnect"));
-            buttonConnect.Click();
-
-           var keywordTextBox = driver.FindElement(By.Id(
-                "contactbook.androidclient:id/editTextKeyword"));
+            buttonConnect.Click();          
+           
+            var keywordTextBox = driver.FindElementById(
+                "contactbook.androidclient:id/editTextKeyword");
             keywordTextBox.Clear();
             keywordTextBox.SendKeys("albert");
 
@@ -49,8 +56,71 @@ namespace QA_Appium_ContactBook
             var lastNameBox = driver.FindElement(By.Id(
                 "contactbook.androidclient:id/textViewLastName"));
 
+            var contactCount = driver.FindElementById("contactbook.androidclient:id/textViewSearchResult").Text;
+
+            Assert.AreEqual("Contacts found: 1", contactCount);
             Assert.AreEqual("Albert", firstNameBox.Text);
             Assert.AreEqual("Einstein", lastNameBox.Text);
+        }
+
+        [Test]
+        public void Search_ValidData_MultipleResults()
+        {
+            var contactBookUrl = driver.FindElement(By.Id(
+                "contactbook.androidclient:id/editTextApiUrl"));
+            contactBookUrl.Clear();
+            contactBookUrl.SendKeys(ApiServiceUrl);
+
+            var buttonConnect = driver.FindElement(By.Id(
+                "contactbook.androidclient:id/buttonConnect"));
+            buttonConnect.Click();
+
+            var keywordTextBox = driver.FindElementById(
+                "contactbook.androidclient:id/editTextKeyword");
+            keywordTextBox.Clear();
+            keywordTextBox.SendKeys("e");
+
+            var connectSearch = driver.FindElement(By.Id(
+              "contactbook.androidclient:id/buttonSearch"));
+            connectSearch.Click();
+            var contactCount = driver.FindElementById("contactbook.androidclient:id/textViewSearchResult").Text;
+
+            Assert.AreEqual("Contacts found: 3", contactCount);
+
+            var firstCellName = driver.FindElementByXPath("//android.widget.TableLayout[1]/android.widget.TableRow[3]/android.widget.TextView[2]").Text;
+
+            var secondCellName = driver.FindElementByXPath("//android.widget.TableLayout[2]/android.widget.TableRow[3]/android.widget.TextView[2]").Text;
+
+            var lastCellName = driver.FindElementByXPath("//android.widget.TableLayout[3]/android.widget.TableRow[3]/android.widget.TextView[2]").Text;
+
+            Assert.That(firstCellName.Contains("e"));
+            Assert.That(secondCellName.Contains("e"));
+            Assert.That(lastCellName.Contains("e"));            
+        }
+
+        [Test]
+        public void Search_InvalidData()
+        {
+            var contactBookUrl = driver.FindElement(By.Id(
+                "contactbook.androidclient:id/editTextApiUrl"));
+            contactBookUrl.Clear();
+            contactBookUrl.SendKeys(ApiServiceUrl);
+
+            var buttonConnect = driver.FindElement(By.Id(
+                "contactbook.androidclient:id/buttonConnect"));
+            buttonConnect.Click();
+
+            var keywordTextBox = driver.FindElementById(
+                "contactbook.androidclient:id/editTextKeyword");
+            keywordTextBox.Clear();
+            keywordTextBox.SendKeys("Aaaaa");
+
+            var connectSearch = driver.FindElement(By.Id(
+              "contactbook.androidclient:id/buttonSearch"));
+            connectSearch.Click();
+            var contactCount = driver.FindElementById("contactbook.androidclient:id/textViewSearchResult").Text;
+
+            Assert.AreEqual("Contacts found: 0", contactCount);
         }
 
         [TearDown]
